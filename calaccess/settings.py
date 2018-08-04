326535da46ba69c4b9b1178f801e6304b1bcc648
palewire <ben.welsh@gmail.com>
2018-08-04 13:05:50 -0700

    Deactivate proxy

--- calaccess/settings.py
+++ calaccess/settings.py
@@ -13,16 +13,16 @@ HTTPCACHE_POLICY = "scrapy.extensions.httpcache.RFC2616Policy"
 
 SPIDER_MIDDLEWARES = {}
 DOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-    'scrapy_proxies.RandomProxy': 100,
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
-    'calaccess.middlewares.RotateUserAgentMiddleware': 120,
+    # 'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    # 'scrapy_proxies.RandomProxy': 100,
+    # 'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+    # 'calaccess.middlewares.RotateUserAgentMiddleware': 120,
 }
 ITEM_PIPELINES = {
     'calaccess.pipelines.JsonPipeline': 300,
 }
 
-RETRY_TIMES = 10
-RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
-PROXY_MODE = 0
-PROXY_LIST = os.path.join(BASE_DIR, 'proxy_list.txt')
+# RETRY_TIMES = 10
+# RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+# PROXY_MODE = 0
+# PROXY_LIST = os.path.join(BASE_DIR, 'proxy_list.txt')
